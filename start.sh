34f0f4add5fb2973034f58d4dd79107dad8bbea4
so1ez <dokrefoz@mail.ru>
2023-11-15 00:01:04 +0300

    added docker compose file and dockerfile for app build

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 10
+sleep 3
 
 echo "sleep is over"
 
@@ -9,5 +9,3 @@ python3 src/database.py
 python3 -m pytest src
 
 uvicorn src.main:app --host 0.0.0.0 --port 8000
-
-echo "script end"

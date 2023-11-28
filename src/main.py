34fef13f8f1fe3dfe0eef1d9df8e6e326a0cef53
so1ez <dokrefoz@mail.ru>
2023-11-28 03:26:26 +0300

    change data format from dict to string, annotations

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@
 
 __author__      = "Boytsov V.M."
 
-from typing import List
+from typing import List, Dict
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException, status
 
 from utils import get_form_template
 from db_utils import get_db_data
+from exceptions import InvalidDataFormatException
 
 app = FastAPI(
     title="GetFormApp",
@@ -16,14 +17,20 @@ app = FastAPI(
 
 
 @app.post("/get_form")
-def get_form(form_query: dict = Body(...)) -> dict:
-    """Returns the name of the found template or typed fields if the template is not found"""
+def get_form(form_query: str = Body(...)) -> Dict[str, str]:
+    """
+        Returns the name of the found template or typed fields if the template is not found.
+        If recieved data have invalid format, raise 
+    """
 
-    return get_form_template(form_query)
+    try:
+        return get_form_template(form_query)
+    except InvalidDataFormatException as exc:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=exc.message) from exc
 
 
 @app.get("/db")
-def get_db() -> List[dict]:
+def get_db() -> List[Dict[str, str]]:
     """Returns all documents in the working collection of a database (list of dicts)"""
 
     return get_db_data()
